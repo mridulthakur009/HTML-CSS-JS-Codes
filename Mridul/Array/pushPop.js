0b2bef86e97ee956619b85b726e39f39fa24c0e2
mridulthakur009 <mridul.thakur.007@gmail.com>
2023-11-18 18:24:13 +0530

    added code

---
+++ Mridul/Array/pushPop.js
@@ -0,0 +1,12 @@
+// array.push : add element at the last index of the array
+
+let arr = ["shasa", "deepak", 55, 40, "ankit"];
+
+// arr.push(1);
+
+console.log(arr);
+
+// array.pop: remove last element (last index) from an array.
+
+arr.pop();
+console.log(arr);
