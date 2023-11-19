8f453860bbeb2d8878e84040101dabe80b7a3f30
mridulthakur009 <mridul.thakur.007@gmail.com>
2023-11-19 11:23:17 +0530

    modified txt

---
+++ Mridul/Array/replacingEleInArr.js
@@ -0,0 +1,11 @@
+let classroom= ["luffy", "zoro", "nami", "sanji", "onepiece"];
+
+// replace zoro with vegita;
+
+classroom[1]= "vegita";
+
+//replacing last element with deepak
+
+classroom[4]= "deepak"
+
+console.log(classroom);
---
+++ Mridul/Array/reverseOfArr.js
@@ -0,0 +1,8 @@
+let arr = [1,2,3,4,5];
+
+let arr2=[];
+
+for(let i=arr.length-1; i>=0; i--){
+     arr2.push(arr[i])
+}
+console.log(arr2);
