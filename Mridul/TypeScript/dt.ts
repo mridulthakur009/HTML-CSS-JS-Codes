ad5c76f3127053d511246cb3dd3090d0d9529463
mridulthakur009 <mridul.thakur.007@gmail.com>
2023-11-22 10:12:31 +0530

    typeScript

---
+++ Mridul/TypeScript/dt.ts
@@ -0,0 +1,15 @@
+let sales: number = 45152422;
+let course: string = "Course";
+let title: boolean=false;
+
+// we can diectly use them without givin them datatype name
+
+let Sales = 45152422;
+let Course = "Course";
+let Title = false;
+
+console.log(sales);
+
+console.log(Sales);
+
+console.log(title);
