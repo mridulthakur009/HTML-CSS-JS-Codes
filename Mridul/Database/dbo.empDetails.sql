5188af244e0b33777dc16fbe58e656e2044d3380
mridulthakur009 <mridul.thakur.007@gmail.com>
2024-01-02 19:04:20 +0530

    added codes

---
+++ Mridul/Database/dbo.empDetails.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[empDetails] (
+    [empNo]   INT            NOT NULL,
+    [Name]    NVARCHAR (50)  NULL,
+    [Email]   NVARCHAR (50)  NULL,
+    [Address] NVARCHAR (150) NULL,
+    PRIMARY KEY CLUSTERED ([empNo] ASC)
+);
+
